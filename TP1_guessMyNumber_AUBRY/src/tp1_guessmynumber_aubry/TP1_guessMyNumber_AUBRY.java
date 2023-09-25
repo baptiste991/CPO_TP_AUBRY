69a2fac105c59dfab04a95db46a99d01bac59ee3
Baptiste AUBRY <baptiste.aubry@epfedu.fr>
2023-09-25 15:27:24 +0200

    Création d'un nouveau projet versionné : exo3 TP1

--- TP1_guessMyNumber_AUBRY/src/tp1_guessmynumber_aubry/TP1_guessMyNumber_AUBRY.java
+++ TP1_guessMyNumber_AUBRY/src/tp1_guessmynumber_aubry/TP1_guessMyNumber_AUBRY.java
@@ -1,20 +1,24 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP1 Exercice 3
+ * Sept 2023
+ * AUBRY Baptiste
  */
 package tp1_guessmynumber_aubry;
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author bapti
- */
 public class TP1_guessMyNumber_AUBRY {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random();
+                
+             int n = generateurAleat.nextInt(100);
+        System.out.println("Saississez un entier entre 0 et 100");
+        
     }
-    
+
 }
