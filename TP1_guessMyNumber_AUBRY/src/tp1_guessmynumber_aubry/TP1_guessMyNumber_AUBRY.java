5e592f5f558aeb864fd179f172e9d474ffc83267
Baptiste AUBRY <baptiste.aubry@epfedu.fr>
2023-09-26 12:42:04 +0200

    Création d'un nouveau projet versionné : exo3 TP1

--- TP1_guessMyNumber_AUBRY/src/tp1_guessmynumber_aubry/TP1_guessMyNumber_AUBRY.java
+++ TP1_guessMyNumber_AUBRY/src/tp1_guessmynumber_aubry/TP1_guessMyNumber_AUBRY.java
@@ -4,6 +4,7 @@
  * AUBRY Baptiste
  */
 package tp1_guessmynumber_aubry;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,22 +16,96 @@ public class TP1_guessMyNumber_AUBRY {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random generateurAleat = new Random();
-                
-             int n = generateurAleat.nextInt(100);
-        System.out.println("Saississez un entier entre 0 et 100");
+        int diff = 0;
+        int intervalle =0;
+        int vie = 30;
+        boolean cauchemar = false;
+
+        System.out.println("Choisissez le niveau de difficulté :");
+        System.out.println("1 Facile");
+        System.out.println("2 Moyen");
+        System.out.println("3 Difficile");
+
+        System.out.println("4 cauchemar");
+        diff = sc.nextInt();
+        do{
+        switch (diff) {
+            case 1:
+                intervalle = 10;
+                vie = 5;
+                break;
+            case 2:
+                intervalle = 50;
+                vie = 10;
+                break;
+            case 3:
+                intervalle = 100;
+                vie = 20;
+                break;
+            case 4:
+                intervalle = 100;
+                vie = 20;
+                cauchemar = true;
+                break;
+            default:
+                System.out.println("Saississez le niveau de difficulté entre 1, 2, 3 et 4");
+                diff = sc.nextInt();
+        }} while (diff <0 || diff >4);
+
+        int n = generateurAleat.nextInt(intervalle);
+        System.out.println("Saississez un entier entre 0 et " + intervalle + ", vous avez " + vie + " essais");
         int nUser = sc.nextInt();
-        
-        while (nUser != n){
-            if (nUser < n)
+        int cpt = 0;
+
+        while (nUser != n && vie > 0 && cauchemar == false) {
+
+            vie -= 1;
+            System.out.println("il vous reste " + vie + " essais");
+
+            if (nUser < n) {
                 System.out.println("Trop petit");
-            else if (nUser > n){
+            } else if (nUser > n) {
                 System.out.println("Trop grand");
+            }
+            // bonne indication
             System.out.println("Saississez un autre nombre :");
             nUser = sc.nextInt();
-            int cpt += 1; 
-            
-            }
-        }
-    }
+            cpt++;
 
+        }
+        while (nUser != n && vie > 0 && cauchemar == true) {
+            int random = generateurAleat.nextInt(9);
+            
+            vie -= 1;
+            System.out.println("il vous reste " + vie + " essais");
+
+            if (random < 3) {
+                
+            //  dans le cas (30% des cas) ou les indications sont mauvaises
+                if (nUser < n) {
+                    System.out.println("Trop grand (cette instruction est peut etre fausse...)");
+                } else if (nUser > n) {
+                    System.out.println("Trop petit (cette instruction est peut etre fausse...)");
+                }
+
+            } else {
+                // indications bonnes
+                if (nUser < n) {
+                    System.out.println("Trop petit (cette instruction est peut etre fausse...)");
+                } else if (nUser > n) {
+                    System.out.println("Trop grand (cette instruction est peut etre fausse...)");
+                }
+            }
+            System.out.println("Saississez un autre nombre :");
+            nUser = sc.nextInt();
+            cpt++;
+        }
+
+        if (vie == 0) {
+            System.out.println("Vous avez perdu, vous avez épuisé votre nombre de vies");
+        } else {
+            System.out.println("Vous avez trouvé le nombre " + n + " en " + cpt + " essais" + " en ayant encore " + vie + " essais !");
+        }
+
+    }
 }
