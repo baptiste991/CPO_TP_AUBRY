158d2abc955be40dc46827a38daf95d35486081f
Baptiste AUBRY <baptiste.aubry@epfedu.fr>
2023-10-13 11:20:33 +0200

    Création d'un nouveau projet versionné : TP3 Héritage

--- TP3_Heritage_AUBRY/src/tp3_heritage_aubry/TP3_Heritage_AUBRY.java
+++ TP3_Heritage_AUBRY/src/tp3_heritage_aubry/TP3_Heritage_AUBRY.java
@@ -16,7 +16,12 @@ public class TP3_Heritage_AUBRY {
      */
     public static void main(String[] args) {
         
-        Epée epee1 = new Epée("Excalibur", 7.5);
+        // création de 2 épées et de 2 batons "tests"
+        Epée epee1 = new Epée("Excalibur", 7, 5);
+        Epée epee2 = new Epée("Durandal", 4, 7);
+        Baton baton1 = new Baton("Chêne", 4, 5 );
+        Baton baton2 = new Baton("Charme", 5, 6 );
+
     }
     
 }
